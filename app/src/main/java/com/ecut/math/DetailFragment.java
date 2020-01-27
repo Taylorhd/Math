8c1be91ebbbdc804d00ee943796f8d4a4d703794
e <2469458627@qq.com>
2020-01-27 18:48:52 +0800

    observer?

--- app/src/main/java/com/ecut/math/DetailFragment.java
+++ app/src/main/java/com/ecut/math/DetailFragment.java
@@ -6,11 +6,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.Observable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.ecut.math.databinding.FragmentDetailBinding;
@@ -22,6 +26,8 @@ import com.ecut.math.databinding.FragmentDetailBinding;
 public class DetailFragment extends Fragment {
     FragmentDetailBinding binding;
     MyViewModel myViewModel;
+    LiveData<String> textQus ;
+    TextView textView ;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -40,10 +46,20 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        textView = getActivity().findViewById(R.id.textView2);
+
         myViewModel = ViewModelProviders.of(getActivity()).get(MyViewModel.class);
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false);
         binding.setData(myViewModel);
         myViewModel.innit();
+        textQus = myViewModel.getShowText();
+//        textQus.observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(String s) {
+//                textView.setText(s);
+//
+//            }
+//        });
         // Inflate the layout for this fragment
         return binding.getRoot();
     }
